1a5958dd44cfe277faf913629e8b0e33fe6f3136
NAGARAJ V <nagarajvellingri2006@gmail.com>
2026-07-21 14:46:42 +0530

    Time: 1 ms (89.05%) | Memory: 19.7 MB (48.14%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.py
@@ -0,0 +1,10 @@
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        m, n = len(grid), len(grid[0])
+        total = m * n
+        k %= total
+        
+        flat = [val for row in grid for val in row]
+        flat = flat[-k:] + flat[:-k]
+        
+        return [flat[i * n:(i + 1) * n] for i in range(m)]
